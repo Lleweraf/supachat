d687806e17b812794c6e6dc2e2c05cf0ca465d58
Lleweraf <67884182+Lleweraf@users.noreply.github.com>
2022-01-24 12:15:38 +0800

    Added scss

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@use "src/variables.scss" as *;',
+          },
+        },
+      },
+    },
+  },
+
+  preprocess: [
+    preprocess({
+      scss: {
+        prependData: '@use "src/variables.scss" as *;',
+      },
+    }),
+  ],
 };
 
 export default config;
