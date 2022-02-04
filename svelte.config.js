07a3cdb1b81adbde9ef2609082aeb7fa09ba1cef
Lleweraf <67884182+Lleweraf@users.noreply.github.com>
2022-02-05 06:12:30 +0800

    style fix

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
     adapter: adapter(),
 
     // hydrate the <div id="svelte"> element in src/app.html
-    //target: "#svelte",
+    //target: '#svelte',
 
     vite: {
       css: {
