f69438514cb54858073253d7765b97351db403a8
Lleweraf <67884182+Lleweraf@users.noreply.github.com>
2022-02-17 22:02:47 +0800

    Added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'supachat',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
